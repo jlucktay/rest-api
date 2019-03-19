17a65c71bf42a2c095378b835170b3320fe46c46
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-19 06:51:29 +0000

    Use ReadAll() in readPayments() handler

--- handlers.read.go
+++ handlers.read.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -11,7 +12,38 @@ import (
 
 func (a *apiServer) readPayments() httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		w.WriteHeader(http.StatusNotImplemented) // 501
+		limit := r.URL.Query().Get("limit")
+		offset := r.URL.Query().Get("offset")
+
+		if limit != "" || offset != "" {
+			w.WriteHeader(http.StatusNotImplemented) // 501
+		}
+
+		allPayments, errRead := a.storage.ReadAll()
+		if errRead != nil {
+			http.Error(
+				w,
+				fmt.Sprintf("Error reading all payments: %s", errRead),
+				http.StatusInternalServerError, // 500
+			)
+			return
+		}
+
+		allBytes, errMarshal := json.Marshal(allPayments)
+		if errMarshal != nil {
+			http.Error(
+				w,
+				fmt.Sprintf("Error marshaling payments: %s", errMarshal),
+				http.StatusInternalServerError, // 500
+			)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK) // 200
+		w.Header().Set("Content-Type", "application/json")
+		if _, errWrite := w.Write(allBytes); errWrite != nil {
+			log.Fatal(errWrite)
+		}
 	}
 }
 
