c9426d605594a8f695b862eda75a8dd553941124
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-21 13:58:35 +0000

    Remove names from interface signature

--- interface.go
+++ interface.go
@@ -9,7 +9,7 @@ import (
 type PaymentStorage interface {
 	Init() error
 	Create(Payment) (uuid.UUID, error)
-	createSpecificId(id uuid.UUID, p Payment) error
+	createSpecificId(uuid.UUID, Payment) error
 	Read(uuid.UUID) (Payment, error)
 	ReadAll() ([]Payment, error)
 	Update(uuid.UUID, Payment) error
