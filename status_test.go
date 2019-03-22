557ebcd99625a8d97a718d4453bece41edac781f
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-22 13:24:21 +0000

    Annotate with error code int

--- status_test.go
+++ status_test.go
@@ -20,79 +20,79 @@ func TestStatusCode(t *testing.T) {
 			desc:     "Create a new payment on a non-existent valid ID",
 			path:     "/payments/60c4feb1-bf67-488a-8d04-627bac487c05",
 			verb:     http.MethodPost,
-			expected: http.StatusNotFound,
+			expected: http.StatusNotFound, // 404
 		},
 		{
 			desc:     "Create a new payment on an invalid ID",
 			path:     "/payments/not-a-valid-v4-uuid",
 			verb:     http.MethodPost,
-			expected: http.StatusNotFound,
+			expected: http.StatusNotFound, // 404
 		},
 		{
 			desc:     "Read the entire collection of existing payments",
 			path:     "/payments",
 			verb:     http.MethodGet,
-			expected: http.StatusOK,
+			expected: http.StatusOK, // 200
 		},
 		{
 			desc:     "Read a non-existent payment at a valid ID",
 			path:     "/payments/29e1c453-8cc7-47b8-9c48-7e44b4f9ba26",
 			verb:     http.MethodGet,
-			expected: http.StatusNotFound,
+			expected: http.StatusNotFound, // 404
 		},
 		{
 			desc:     "Read a non-existent payment at an invalid ID",
 			path:     "/payments/not-a-valid-v4-uuid",
 			verb:     http.MethodGet,
-			expected: http.StatusNotFound,
+			expected: http.StatusNotFound, // 404
 		},
 		{
 			desc:     "Update all existing payments",
 			path:     "/payments",
 			verb:     http.MethodPut,
-			expected: http.StatusMethodNotAllowed,
+			expected: http.StatusMethodNotAllowed, // 405
 		},
 		{
 			desc:     "Update an existing payment",
 			path:     "/payments/67191210-3e30-40c9-af61-3f2abb110363",
 			verb:     http.MethodPut,
-			expected: http.StatusNoContent,
+			expected: http.StatusNoContent, // 204
 		},
 		{
 			desc:     "Update a non-existent payment at a valid ID",
 			path:     "/payments/ac5f6fcd-8e69-4949-ad93-d15c51991802",
 			verb:     http.MethodPut,
-			expected: http.StatusNotFound,
+			expected: http.StatusNotFound, // 404
 		},
 		{
 			desc:     "Update a non-existent payment at an invalid ID",
 			path:     "/payments/not-a-valid-v4-uuid",
 			verb:     http.MethodPut,
-			expected: http.StatusNotFound,
+			expected: http.StatusNotFound, // 404
 		},
 		{
 			desc:     "Delete all existing payments",
 			path:     "/payments",
 			verb:     http.MethodDelete,
-			expected: http.StatusMethodNotAllowed,
+			expected: http.StatusMethodNotAllowed, // 405
 		},
 		{
 			desc:     "Delete an existing payment at a valid ID",
 			path:     "/payments/a300eb47-efe0-44b0-b729-bed75123bf3a",
 			verb:     http.MethodDelete,
-			expected: http.StatusOK,
+			expected: http.StatusOK, // 200
 		},
 		{
 			desc:     "Delete a non-existent payment at a valid ID",
 			path:     "/payments/943c4811-f66a-4cb1-8d5b-3ed7da0ce934",
 			verb:     http.MethodDelete,
-			expected: http.StatusNotFound,
+			expected: http.StatusNotFound, // 404
 		},
 		{
 			desc:     "Delete a non-existent payment at an invalid ID",
 			path:     "/payments/not-a-valid-v4-uuid",
 			verb:     http.MethodDelete,
-			expected: http.StatusNotFound,
+			expected: http.StatusNotFound, // 404
 		},
 	}
 
