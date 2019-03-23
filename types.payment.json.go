934458cdab11190dc8d9c300debf1d251bf2b3c8
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-23 06:47:36 +0000

    Implement JSON interfaces for Payment type

---
+++ types.payment.json.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+)
+
+func (this Payment) MarshalJSON() ([]byte, error) {
+	buffer := new(bytes.Buffer)
+	j, err := json.Marshal(this)
+	if err != nil {
+		return nil, err
+	}
+	buffer.WriteString(string(j))
+
+	return buffer.Bytes(), nil
+}
+
+func (this Payment) UnmarshalJSON(b []byte) error {
+	var p Payment
+	err := json.Unmarshal(b, &p)
+	if err != nil {
+		return err
+	}
+	return nil
+}
