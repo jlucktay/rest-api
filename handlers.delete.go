e94e530e47b299a6ae1e81ad048585640c722aa7
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-23 07:48:01 +0000

    Implement delete handler

--- handlers.delete.go
+++ handlers.delete.go
@@ -4,10 +4,23 @@ import (
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	uuid "github.com/satori/go.uuid"
 )
 
 func (a *apiServer) deletePaymentByID() httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		w.WriteHeader(http.StatusNotImplemented)
+		id := uuid.FromStringOrNil(p.ByName("id"))
+
+		if id == uuid.Nil {
+			http.Error(w, "Invalid ID.", http.StatusNotFound) // 404
+			return
+		}
+
+		if errDelete := a.storage.Delete(id); errDelete == nil {
+			w.WriteHeader(http.StatusOK) // 200
+			return
+		}
+
+		http.Error(w, (&NotFoundError{id}).Error(), http.StatusNotFound) // 404
 	}
 }
