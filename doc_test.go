1310ecf43e62298cb5e18fc7fa2cb2af71a20941
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-28 15:17:47 +0000

    Hard-code ID into testdata.

--- doc_test.go
+++ doc_test.go
@@ -13,6 +13,11 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
+type testDataWrapper struct {
+	Attributes Payment   `json:"attributes"`
+	ID         uuid.UUID `json:"id"`
+}
+
 func TestDocumentationSingle(t *testing.T) {
 	// Set up an API server to test against.
 	srv := newAPIServer(InMemory)
@@ -22,15 +27,14 @@ func TestDocumentationSingle(t *testing.T) {
 	// Put the single payment from the documentation into the server.
 	singleBytes, errReadFile := ioutil.ReadFile("testdata/doc.single.json")
 	i.NoErr(errReadFile)
-	var single Payment
+	var single testDataWrapper
 	errUmSingle := json.Unmarshal(singleBytes, &single)
 	i.NoErr(errUmSingle)
-	existingID := uuid.Must(uuid.NewV4())
-	errCreate := srv.storage.createSpecificID(existingID, single)
+	errCreate := srv.storage.createSpecificID(single.ID, single.Attributes)
 	i.NoErr(errCreate)
 
 	// Do a HTTP request for the single payment.
-	req, errReq := http.NewRequest(http.MethodGet, fmt.Sprintf("/payments/%s", existingID), nil)
+	req, errReq := http.NewRequest(http.MethodGet, fmt.Sprintf("/payments/%s", single.ID), nil)
 	i.NoErr(errReq)
 	srv.router.ServeHTTP(w, req)
 	i.Equal(http.StatusOK, w.Result().StatusCode)
@@ -38,7 +42,7 @@ func TestDocumentationSingle(t *testing.T) {
 	// Put info from the ./testdata/ JSON file into a wrapper struct.
 	var expected readWrapper
 	expected.init(req)
-	expected.addPayment(existingID, single)
+	expected.addPayment(single.ID, single.Attributes)
 
 	// Assert that it matches the JSON returned by the API.
 	responseBytes, errReadResponse := ioutil.ReadAll(w.Result().Body)
