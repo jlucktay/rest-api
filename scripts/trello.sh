6fffd245bd9e6fd6d2ccb0f08b83a56fa17bd4ea
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-30 14:03:57 +0000

    Add checking for Trello secrets, and an example file.

--- scripts/trello.sh
+++ scripts/trello.sh
@@ -15,10 +15,17 @@ if ! command -v jq > /dev/null; then
     exit 1
 fi
 
-Board=$(jq -r '.board' secrets.trello.json)
-FinishedListId=$(jq -r '.finishedListId' secrets.trello.json)
-Key=$(jq -r '.key' secrets.trello.json)
-Token=$(jq -r '.token' secrets.trello.json)
+SecretsFile="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/secrets.trello.json"
+
+if ! [[ -f $SecretsFile ]]; then
+    echo "The Trello secrets file '$SecretsFile' does not exist. See the example JSON file alongside this script."
+    exit 1
+fi
+
+Board=$(jq -r '.board' "$SecretsFile")
+FinishedListId=$(jq -r '.finishedListId' "$SecretsFile")
+Key=$(jq -r '.key' "$SecretsFile")
+Token=$(jq -r '.token' "$SecretsFile")
 
 URL="https://api.trello.com/1/boards/$Board/cards/?key=$Key&token=$Token"
 
