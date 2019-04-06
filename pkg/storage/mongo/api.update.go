f9944d0b0dca55abcbcb4cc6f65c44f579f62064
James Lucktaylor <jlucktay+github@gmail.com>
2019-04-06 07:22:19 +0100

    Implement update.

--- pkg/storage/mongo/api.update.go
+++ pkg/storage/mongo/api.update.go
@@ -1,12 +1,20 @@
 package mongo
 
 import (
-	"errors"
+	"context"
 
 	"github.com/jlucktay/rest-api/pkg/storage"
 	uuid "github.com/satori/go.uuid"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (s *Storage) Update(id uuid.UUID, p storage.Payment) error {
-	return errors.New("not yet implemented")
+	filter := bson.D{{Key: "_id", Value: id.String()}}
+	mongoUpdate := bson.M{"$set": wrap(p, id)}
+	_, errUpdate := s.coll.UpdateOne(context.TODO(), filter, mongoUpdate)
+	if errUpdate != nil {
+		return errUpdate
+		// return &storage.NotFoundError{ID: id} // todo?
+	}
+	return nil
 }
