20c7cc9c2ba27079f346670a9b5b639070d79878
James Lucktaylor <jlucktay+github@gmail.com>
2019-04-06 07:23:03 +0100

    Implement delete.

--- pkg/storage/mongo/api.delete.go
+++ pkg/storage/mongo/api.delete.go
@@ -1,11 +1,17 @@
 package mongo
 
 import (
-	"errors"
+	"context"
 
 	uuid "github.com/satori/go.uuid"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (s *Storage) Delete(id uuid.UUID) error {
-	return errors.New("not yet implemented")
+	filter := bson.D{{Key: "_id", Value: id.String()}}
+	_, errDelete := s.coll.DeleteOne(context.TODO(), filter)
+	if errDelete != nil {
+		return errDelete
+	}
+	return nil
 }
