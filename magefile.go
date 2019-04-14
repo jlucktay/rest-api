76e353eb043c688315fec867b73389e165bbac39
James Lucktaylor <jlucktay+github@gmail.com>
2019-04-14 08:46:51 +0100

    Add more Mage targets. Docker running OK now.

--- magefile.go
+++ magefile.go
@@ -16,32 +16,46 @@ func Test() error {
 }
 
 // Lint will check the Dockerfile and Go files for errors.
-func Lint() error {
-	if errDockerLint := sh.RunV("hadolint", "build/Dockerfile"); errDockerLint != nil {
-		return errDockerLint
-	}
+func Lint() {
+	mg.Deps(LintDocker, LintGo)
+}
+
+// LintDocker lints the Dockerfile.
+func LintDocker() error {
+	return sh.Run("hadolint", "build/Dockerfile")
+}
+
+// LintGo lints all Go files.
+func LintGo() error {
 	return sh.Run("golangci-lint", "run", "--enable-all")
 }
 
-// Dock will build the Docker image containing the REST API.
-func Dock() error {
+// Build will compile the REST API binary locally.
+func Build() error {
+	mg.Deps(Clean)
+	return sh.Run("go", "build", "-a", "-installsuffix", "cgo", "-ldflags", `-extldflags "-static"`, "-o", "rest-api", "./cmd/api")
+}
+
+// DockerBuild will build the Docker image, which executes a layered build of the REST API.
+func DockerBuild() error {
 	//     --label list              Set metadata for an image // todo?
 	return sh.Run("docker", "build", "--file", "build/Dockerfile", "--tag", "jlucktay/rest-api", ".")
 }
 
-// Build will compile the REST API binary.
-func Build() error {
-	return sh.Run("go", "build", "-a", "-installsuffix", "cgo", "-ldflags", `-extldflags "-static"`, "-o", "main", "./cmd/api")
+// DockerRun will run the Docker image.
+func DockerRun() error {
+	mg.Deps(DockerBuild)
+	return sh.RunV("docker", "run", "--publish", "8080:8080", "jlucktay/rest-api")
 }
 
 // Clean will removed compiled binaries.
 func Clean() error {
-	return sh.Run("rm", "-fv", "main")
+	return sh.Run("rm", "-fv", "rest-api")
 }
 
 // Full runs all targets; linting and testing in parallel, then the Docker build.
 func Full() {
-	mg.Deps(Clean)
 	mg.Deps(Lint, Test)
-	mg.Deps(Build, Dock)
+	mg.Deps(Build, DockerBuild)
+	mg.Deps(DockerRun)
 }
