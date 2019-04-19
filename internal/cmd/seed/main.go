d52b15cd42078ce2f8d88e52736c5c608d290f1d
James Lucktaylor <jlucktay+github@gmail.com>
2019-04-19 08:35:48 +0100

    Roll helper func into error catching.

--- internal/cmd/seed/main.go
+++ internal/cmd/seed/main.go
@@ -27,57 +27,46 @@ func main() {
 	fmt.Println("Continuing will delete ALL payment records in MongoDB (database: rest-api, collection: payments")
 	fmt.Print("Press 'Enter' to continue, or CTRL+C to cancel...")
 	_, errRead := bufio.NewReader(os.Stdin).ReadBytes('\n')
-	theShowMustGoOn("error reading from stdin:", errRead)
+	theShowMustGoOn("error reading from stdin", errRead)
 
 	// Empty out current MongoDB collection.
 	m := mongo.New()
-	if errInit := m.Initialise(); errInit != nil {
-		log.Fatal("error initialising MongoDB the first time:", errInit)
-	}
-	if errTerm := m.Terminate(true); errTerm != nil {
-		log.Fatal("error emptying MongoDB:", errTerm)
-	}
+	errInit := m.Initialise()
+	theShowMustGoOn("error initialising MongoDB the first time", errInit)
+
+	errTerm := m.Terminate(true)
+	theShowMustGoOn("error emptying MongoDB", errTerm)
 
 	// Renew our MongoDB client's connection to the database.
 	m = mongo.New()
-	if errInitAgain := m.Initialise(); errInitAgain != nil {
-		log.Fatal("error initialising MongoDB the second time:", errInitAgain)
-	}
+	errInitAgain := m.Initialise()
+	theShowMustGoOn("error initialising MongoDB the second time", errInitAgain)
 
 	// Get the list of payments from Mockbin.
-	seedUrl, errParse := url.Parse("http://mockbin.org/bin/41ca3269-d8c4-4063-9fd5-f306814ff03f")
-	if errParse != nil {
-		log.Fatal("error parsing URL:", errParse)
-	}
-	resp, errGet := getResponse(*seedUrl)
-	if errGet != nil {
-		log.Fatal("error getting response:", errGet)
-	}
+	seedURL, errParse := url.Parse("http://mockbin.org/bin/41ca3269-d8c4-4063-9fd5-f306814ff03f")
+	theShowMustGoOn("error parsing URL", errParse)
+
+	resp, errGet := getResponse(*seedURL)
+	theShowMustGoOn("error getting response", errGet)
 
 	respBytes, errRead := ioutil.ReadAll(resp)
-	if errRead != nil {
-		log.Fatal("error reading bytes:", errRead)
-	}
+	theShowMustGoOn("error reading bytes", errRead)
 
 	// Parse the payments out into a struct.
 	rw := &server.ReadWrapper{}
-
-	if errUm := json.Unmarshal(respBytes, rw); errUm != nil {
-		log.Fatal("error unmarshaling response:", errUm)
-	}
+	errUm := json.Unmarshal(respBytes, rw)
+	theShowMustGoOn("error unmarshaling response", errUm)
 
 	// Create the individual payments in storage, and preserve their UUIDs.
 	for _, pd := range rw.Data {
 		errCreate := m.CreateSpecificID(pd.ID, pd.Attributes)
-		if errCreate != nil {
-			log.Fatal("error creating payment:", errCreate)
-		}
+		theShowMustGoOn("error creating payment", errCreate)
 		fmt.Printf("Added payment with ID '%v'.\n", pd.ID)
 	}
 }
 
 func theShowMustGoOn(s string, e error) {
 	if e != nil {
-		log.Fatal(s, e)
+		log.Fatalf("%s: %s\n", s, e)
 	}
 }
