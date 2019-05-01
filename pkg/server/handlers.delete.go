c99d6ad8669126e3e6bbc2136e347e61e4e03a58
James Lucktaylor <jlucktay+github@gmail.com>
2019-05-01 20:38:31 +0100

    (Re)wire some more logging.

--- pkg/server/handlers.delete.go
+++ pkg/server/handlers.delete.go
@@ -8,20 +8,23 @@ import (
 	"github.com/jlucktay/rest-api/pkg/storage"
 )
 
-func (a *Server) deletePaymentByID() http.HandlerFunc {
+func (s *Server) deletePaymentByID() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		id := uuid.FromStringOrNil(chi.URLParam(r, "id"))
 
 		if id == uuid.Nil {
+			s.Log.Debug("ID was invalid, returning.")
 			http.Error(w, "Invalid ID.", http.StatusNotFound) // 404
 			return
 		}
 
-		if errDelete := a.Storage.Delete(id); errDelete == nil {
+		if errDelete := s.Storage.Delete(id); errDelete == nil {
+			s.Log.Debugf("Successfully deleted record with given ID '%s'.", id)
 			w.WriteHeader(http.StatusOK) // 200
 			return
 		}
 
+		s.Log.Debug("ID was not found, returning.")
 		http.Error(w, (&storage.NotFoundError{ID: id}).Error(), http.StatusNotFound) // 404
 	}
 }
