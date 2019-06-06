cdcd51a2926c61e2fd03a3722c2cb4843ae4a8a7
James Lucktaylor <jlucktay+github@gmail.com>
2019-06-06 11:40:58 +0100

    Add godoc/notes to main package.

--- mage.vscode.go
+++ mage.vscode.go
@@ -1,5 +1,7 @@
 // +build vscode
 
+// main makes sure, in tandem with the configuration under '.vscode/settings.json' also in this repo, that VSCode
+// will build, lint, statically analyse, etc etc the 'magefile.go' file.
 package main
 
 func main() {}
