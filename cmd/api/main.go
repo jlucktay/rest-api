27417d53f314495e4a6bb80614b95acc4833e8db
James Lucktaylor <jlucktay@users.noreply.github.com>
2019-06-15 19:12:50 +0100

    Docker compose (#21)
    * Updated and tidied dependencies.
    * Created a docker-compose.yml prototype file.
    * Dockerfile:
      * Switched over to using Go modules.
      * Consolidated build flags.
    * Added some more docs and notes.
    * Wired up Mongo host flag via optional env var.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,13 +5,22 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/jlucktay/rest-api/pkg/server"
 )
 
+const defaultMongo = "localhost"
+
 func main() {
-	var mongoHostname = flag.String("mongo-host", "localhost", "the hostname of the MongoDB server to connect to")
+	var mongoHostname = flag.String("mongo-host", defaultMongo, "the hostname of the MongoDB server to connect to")
 	flag.Parse()
+
+	// If the flag has not been given, look for an environment variable.
+	if envMongo, found := os.LookupEnv("MONGO_HOST"); found && *mongoHostname == defaultMongo {
+		*mongoHostname = envMongo
+	}
+
 	s := server.New(server.Mongo, *mongoHostname)
 	log.Fatal(http.ListenAndServe(":8080", s.Router))
 }
