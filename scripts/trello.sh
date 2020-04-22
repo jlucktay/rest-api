dafc015b75f72405511c9a09284902a097361686
James Lucktaylor <jlucktay+github@gmail.com>
2020-04-22 10:43:12 +0100

    Autoformat.

--- scripts/trello.sh
+++ scripts/trello.sh
@@ -3,15 +3,15 @@ set -euo pipefail
 IFS=$'\n\t'
 
 if ! command -v jq > /dev/null; then
-    echo "'jq' not found! Please install: https://stedolan.github.io/jq/download/"
-    exit 1
+  echo "'jq' not found! Please install: https://stedolan.github.io/jq/download/"
+  exit 1
 fi
 
 SecretsFile="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/secrets.trello.json"
 
 if ! [[ -f $SecretsFile ]]; then
-    echo "The Trello secrets file '$SecretsFile' does not exist. See the example JSON file alongside this script."
-    exit 1
+  echo "The Trello secrets file '$SecretsFile' does not exist. See the example JSON file alongside this script."
+  exit 1
 fi
 
 Board=$(jq -r '.board' "$SecretsFile")
