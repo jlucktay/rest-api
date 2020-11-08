ab8266a018227218f0395c8fcc8565ba680e3e9b
James Lucktaylor <jlucktay+github@gmail.com>
2020-11-08 11:36:45 +0000

    refactor(pkg/server): update constructor arg to take a connection string

--- pkg/server/constructor_test.go
+++ pkg/server/constructor_test.go
@@ -9,29 +9,36 @@ import (
 	"go.jlucktay.dev/rest-api/pkg/server"
 	"go.jlucktay.dev/rest-api/pkg/storage"
 	"go.jlucktay.dev/rest-api/pkg/storage/inmemory"
-	"go.jlucktay.dev/rest-api/pkg/storage/mongo"
+	jramongo "go.jlucktay.dev/rest-api/pkg/storage/mongo"
 )
 
 func TestNew(t *testing.T) {
 	testCases := map[string]struct {
-		st       server.StorageType
-		expected storage.PaymentStorage
+		st         server.StorageType
+		expected   storage.PaymentStorage
+		connString string
 	}{
 		"In-memory storage (map); won't persist across app restarts": {
 			st:       server.InMemory,
 			expected: &inmemory.Storage{},
 		},
 		"Database storage (MongoDB); will persist across app restarts": {
-			st:       server.Mongo,
-			expected: &mongo.Storage{},
+			st:         server.Mongo,
+			expected:   &jramongo.Storage{},
+			connString: directConnString,
 		},
 	}
+
 	for name, tC := range testCases {
 		tC := tC // pin!
 
-		t.Run(name, func(t *testing.T) {
-			is := is.New(t)
-			s := server.New(tC.st, true)
+		t.Run(name, func(_ *testing.T) {
+			var s *server.Server
+			if tC.connString != "" {
+				s = server.New(tC.st, true, tC.connString)
+			} else {
+				s = server.New(tC.st, true)
+			}
 			is.Equal(reflect.TypeOf(s.Storage), reflect.TypeOf(tC.expected)) // wrong storage type
 		})
 	}
