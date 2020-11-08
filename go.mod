2b241a48559627c84d3515e8db1e807e3e5b2571
James Lucktaylor <jlucktay+github@gmail.com>
2020-11-08 11:38:18 +0000

    test(pkg/server): use dockertest for MongoDB

--- go.mod
+++ go.mod
@@ -3,12 +3,25 @@ module go.jlucktay.dev/rest-api
 go 1.14
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
+	github.com/Microsoft/go-winio v0.4.15 // indirect
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/containerd/continuity v0.0.0-20200928162600-f2cc35102c2a // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
 	github.com/go-chi/chi v4.1.2+incompatible
 	github.com/gofrs/uuid v3.3.0+incompatible
 	github.com/google/go-cmp v0.5.2
+	github.com/gotestyourself/gotestyourself v2.2.0+incompatible // indirect
+	github.com/lib/pq v1.8.0 // indirect
 	github.com/magefile/mage v1.10.0
 	github.com/matryer/is v1.4.0
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/opencontainers/runc v0.1.1 // indirect
+	github.com/ory/dockertest v3.3.5+incompatible
 	github.com/sirupsen/logrus v1.6.0
 	github.com/xdg/stringprep v1.0.0 // indirect
 	go.mongodb.org/mongo-driver v1.4.1
+	gotest.tools v2.2.0+incompatible // indirect
 )
--- pkg/server/constructor_test.go
+++ pkg/server/constructor_test.go
@@ -1,10 +1,15 @@
 package server_test
 
 import (
+	"context"
+	"fmt"
 	"reflect"
 	"testing"
 
 	"github.com/matryer/is"
+	"github.com/ory/dockertest"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"go.jlucktay.dev/rest-api/pkg/server"
 	"go.jlucktay.dev/rest-api/pkg/storage"
@@ -13,6 +18,44 @@ import (
 )
 
 func TestNew(t *testing.T) {
+	is := is.New(t)
+
+	// Set up a disposable MongoDB container with Docker
+	t.Log("Docker/MongoDB starting...")
+	dockerPool, errPool := dockertest.NewPool("")
+	is.NoErr(errPool)
+
+	mongoResource, errRun := dockerPool.Run("mongo", "4", nil)
+	is.NoErr(errRun)
+
+	mongoContainerName := mongoResource.Container.Name
+
+	defer func() {
+		t.Logf("Purging Docker/MongoDB container '%s'...", mongoContainerName)
+		is.NoErr(dockerPool.Purge(mongoResource))
+		t.Logf("Purged Docker/MongoDB container '%s'!", mongoContainerName)
+	}()
+
+	directConnString := fmt.Sprintf(
+		"mongodb://localhost:%s/jra_test",
+		mongoResource.GetPort("27017/tcp"))
+
+	// Exponential backoff-retry, while MongoDB gets ready to accept connections
+	if err := dockerPool.Retry(func() error {
+		mgoOpts := (&options.ClientOptions{}).ApplyURI(directConnString)
+
+		mgoClient, errConnect := mongo.Connect(context.TODO(), mgoOpts)
+		if errConnect != nil {
+			return errConnect
+		}
+
+		return mgoClient.Ping(context.TODO(), nil)
+	}); err != nil {
+		is.NoErr(err) // could not ping Docker/MongoDB
+	}
+
+	t.Logf("Started Docker/MongoDB container '%s'!", mongoContainerName)
+
 	testCases := map[string]struct {
 		st         server.StorageType
 		expected   storage.PaymentStorage
